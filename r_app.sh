003826eb34ce456bead7bf6ada7b0eb21d9570b6
simplejoy <lantian-lz@163.com>
2014-05-30 15:53:29 +0800

    add file

---
+++ r_app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ x$1 == x ]; then
+   echo 'which app?'
+   exit 1;
+fi
+ports=`ps aux | grep $1: | grep -v grep | gawk '{print $NF}' | sed 's/:.../:/'`
+for port in ${ports[*]}
+do
+    supervisorctl restart $port
+done
