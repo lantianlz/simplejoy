590d634d07da473c19554a31e39ea87e0029a2ca
simplejoy <lantian-lz@163.com>
2014-05-31 22:08:41 +0800

    修改为绝对路径

--- www/settings.py
+++ www/settings.py
@@ -28,7 +28,7 @@ if not LOCAL_FLAG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'blog_db',  # Or path to database file if using sqlite3.
+            'NAME': '/var/www/simplejoy/blog_db',  # Or path to database file if using sqlite3.
             'USER': '',  # Not used with sqlite3.
             'PASSWORD': '',  # Not used with sqlite3.
             'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
